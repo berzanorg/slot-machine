66ff470ae338d74736a28fe059d0219aa6705db3
berzan <berzan@monda.exchange>
2025-05-08 14:57:53 +0900

    format

--- src/Counter.sol
+++ src/Counter.sol
@@ -6,6 +6,8 @@ contract Counter {
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+        address b = 0x0000000000000000000000000000000000000000;
+        uint256 supply = 23_423_242;
     }
 
     function increment() public {
