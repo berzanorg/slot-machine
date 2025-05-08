240a8ed52e4467e6752199121890d8650c07148d
berzan <berzan@monda.exchange>
2025-05-08 17:26:55 +0900

    reimplement

--- src/SlotMachine.sol
+++ src/SlotMachine.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract SlotMachine {
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { IERC721Receiver } from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract SlotMachine is IERC721Receiver {
     event Win(address indexed player, address indexed collection, uint256 id);
     event Put(address indexed collection, uint256 id);
 
@@ -22,45 +25,47 @@ contract SlotMachine {
     }
 
     function getRandom() internal view returns (uint256) {
-        return block.timestamp / itemsCount;
+        return block.timestamp % itemsCount;
     }
 
     function spin() external payable {
-        address player = msg.sender;
-
         require(msg.value == cost, CostNotPaid());
 
-        require(items.length != 0, EmptyInventory());
+        require(itemsCount != 0, EmptyInventory());
 
-        while (true) {
-            uint256 random = getRandom();
+        for (uint256 i = 0;; i++) {
+            uint256 random = getRandom(); // get another random number when that item is already given
             Item memory item = items[random];
             if (item.collection == address(0)) continue;
             emptySlots.push(random);
             delete items[random];
+            IERC721(item.collection).transferFrom(address(this), msg.sender, uint256(item.id));
+
+            emit Win(msg.sender, item.collection, item.id);
+
             itemsCount--;
-            // send nft
-            emit Win(player, item.collection, item.id);
+
+            break;
         }
     }
 
-    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data)
+    function onERC721Received(address, address, uint256 tokenId, bytes calldata)
         external
         returns (bytes4)
     {
         require(tokenId <= type(uint96).max);
         if (emptySlots.length == 0) {
-            items.push(Item({ collection: operator, id: uint96(tokenId) }));
+            items.push(Item({ collection: msg.sender, id: uint96(tokenId) }));
         } else {
             uint256 slot = emptySlots[emptySlots.length - 1];
             emptySlots.pop();
-            items[slot] = Item({ collection: operator, id: uint96(tokenId) });
+            items[slot] = Item({ collection: msg.sender, id: uint96(tokenId) });
         }
 
         itemsCount++;
 
-        emit Put(operator, tokenId);
+        emit Put(msg.sender, tokenId);
 
-        // return selector
+        return IERC721Receiver.onERC721Received.selector;
     }
 }
